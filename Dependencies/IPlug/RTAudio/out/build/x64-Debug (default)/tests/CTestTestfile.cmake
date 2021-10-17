8c4c1337836a22ad0d064049574cc138288802f3
Roberto A. Ramirez Gonzalez <roberto.a.ramirezgonzalez065@gmail.com>
2021-10-17 00:05:56 -0500

    update git ignore

--- Dependencies/IPlug/RTAudio/out/build/x64-Debug (default)/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/audio-plugins/iPlug2/Dependencies/IPlug/RTAudio/tests
-# Build directory: D:/audio-plugins/iPlug2/Dependencies/IPlug/RTAudio/out/build/x64-Debug (default)/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(apinames "D:/audio-plugins/iPlug2/Dependencies/IPlug/RTAudio/out/build/x64-Debug (default)/tests/apinames.exe")
-set_tests_properties(apinames PROPERTIES  _BACKTRACE_TRIPLES "D:/audio-plugins/iPlug2/Dependencies/IPlug/RTAudio/tests/CMakeLists.txt;32;add_test;D:/audio-plugins/iPlug2/Dependencies/IPlug/RTAudio/tests/CMakeLists.txt;0;")
